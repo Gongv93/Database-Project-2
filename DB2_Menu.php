93d37c6f1ab955f17b93d18b0397540845aac18d
Vincent Gong <gongv93@gmail.com>
2014-04-17 23:53:18 -0400

    Update SQL 1

--- DB2_Menu.php
+++ DB2_Menu.php
@@ -3,15 +3,35 @@
 <html>
 	<head>
 		<title>Database</title>
+	</head>
+	<body>
 		<?php 
-            if(!@mysql_connect("134.74.112.65", "gon14", "vincent", "d409")) { 
+            if(!@mysql_connect("134.74.112.65", "gon14", "vincent")) { 
                 echo "<h2>Connection Error.</h2>"; 
                 die(); 
             } 
-            mysql_select_db("coursedemo"); 
+            mysql_select_db("d409"); 
         ?>
-	</head>
-	<body>
-		<?php echo "<p>Hello World</p>" ; ?>  
+		<?php
+		$strSQL = "SELECT controlArea
+				   FROM ControlArea CA
+				   WHERE CA.controlareaID IN
+				 ( SELECT controlAreaID 
+				   FROM TurnStiles 
+				   GROUP BY controlAreaID 
+				   HAVING COUNT(controlAreaID) >15
+				  )";
+		
+		$rs = mysql_query($strSQL);
+		
+		while($row = mysql_fetch_array($rs)) {
+
+		echo $row['controlArea']."<br/>";
+
+		}
+
+	mysql_close();
+	?>		
 	</body>
 </html>
+
